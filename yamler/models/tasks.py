904d842a9dadb26167fd9593708bb263cadbda82
yanghuzhi <huzhi.yang@foxmail.com>
2012-04-12 14:40:16 +0800

    add:task create/get interface

--- yamler/models/tasks.py
+++ yamler/models/tasks.py
@@ -1,15 +1,30 @@
 #encoding:utf8
-
+import datetime
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from yamler.database import Model  
+from yamler.database import Model 
+from werkzeug import http_date
 
 class Task(Model):
+    __tablename__ = 'tasks'
     id = Column(Integer, primary_key=True)
     title = Column(String(150))
-    note = Column(String(200))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    created_at = Column(datetime,default=datetime.datetime.now()) 
-    updated_at = Column(datetime,default=datetime.datetime.now()) 
+    user_id = Column(Integer, ForeignKey('users.id'))
+    note = Column(String(200),default='')
+    priority = Column(Integer, default=1)
+    end_time = Column(Integer, default='')
+    created_at = Column(DateTime, default=datetime.datetime.now()) 
+    updated_at = Column(DateTime,default=datetime.datetime.now()) 
 
-    def __init__(sefl):
-        pass
+    def __init__(self, title, user_id, note=None, priority=None, end_time=None):
+        self.title = title
+        self.note = note
+        self.user_id = user_id
+        self.priority = priority
+        self.end_time = end_time
+
+
+    def __repr__(self):
+        return '<Task %r>' % (self.title)
+
+    def to_json(self):
+         return dict(title = self.title, note = self.note, user_id = self.user_id, priority = self.priority, end_time = http_date(self.end_time),create_time = http_date(self.created_at)) 
--- yamler/views/mobile.py
+++ yamler/views/mobile.py
@@ -2,6 +2,9 @@
 from flask import Blueprint, request, session, jsonify
 from yamler.database import db_session
 from yamler.models.users import User
+from yamler.models.tasks import Task
+from yamler.utils import to_json
+from sqlalchemy.sql import between
 
 mod = Blueprint('mobile',__name__,url_prefix='/mobile')
 
@@ -25,7 +28,7 @@ def register():
     username = request.form['username']
     password = request.form['password']
     if username and password:
-        user = User(username,password,is_active=1)
+        user = User(username, password, is_active = 1)
         result = User.query.filter_by(username = user.username).first() 
         if result:
             return jsonify(error=1, code='username_exists', message='用户名已经存在')
@@ -35,3 +38,25 @@ def register():
             return jsonify(error=0, code='success', message='成功注册')
 
     return jsonify(error=1, code = 'no_username_or_password', message='没有输入用户名或密码')
+
+@mod.route('/task/create',methods=['POST'])
+def task_create():
+    if request.form['user_id'] and request.form['title']:
+        task = Task(title = request.form['title'], 
+                    user_id = request.form['user_id'], 
+                    note = request.form['note'] if request.form.has_key('note') else '', 
+                    priority = request.form['priority'] if request.form.has_key('priority') else 1, 
+                    end_time = request.form['end_time'] if request.form.has_key('end_time') else 1,
+                    ) 
+        db_session.add(task)
+        db_session.commit()
+        return jsonify(error=0, code='success', message='添加成功')
+    return jsonify(error=1, code='failed', message='输入数据不合法')
+
+@mod.route('/task/get',methods=['POST', 'GET'])
+def task_get():
+    if request.form['user_id'] and request.form['start_time'] and request.form['end_time']:
+        rows = db_session.query(Task).filter(between(Task.created_at, request.form['start_time'], request.form['end_time'])).filter_by(user_id = request.form['user_id']).all()
+        data = [row.to_json() for row in rows]
+        return jsonify(error = 0, data = data)
+    return jsonify(error = 1, data = {}) 
