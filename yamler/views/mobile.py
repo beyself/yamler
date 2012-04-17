73be040d0475c64e4f9bebb4833eddc40dc59615
yanghuzhi <huzhi.yang@foxmail.com>
2012-04-17 15:21:13 +0800

    !

--- yamler/views/mobile.py
+++ yamler/views/mobile.py
@@ -16,9 +16,9 @@ def login():
         result = User.query.filter_by(username=user.username, password=user.password).first()
         if result:
             session['user_id'] = result.id
-            return jsonify(error=0, code='success', message='登录成功')
+            return jsonify(error=0, code='success', message='登录成功', user_id = result.id)
         else:
-            return jsonify(error=1, code='username_or_password_error',message='用户名或密码错误')
+            return jsonify(error=1, code='username_or_password_error',message='用户名或密码错误',)
     else:
         return jsonify(error=1, code='no_username_or_password', message='没有输入用户名或密码')
 
@@ -34,7 +34,7 @@ def register():
         else:
             db_session.add(user)
             db_session.commit()
-            return jsonify(error=0, code='success', message='成功注册')
+            return jsonify(error=0, code='success', message='成功注册', user_id = user.id)
 
     return jsonify(error=1, code = 'no_username_or_password', message='没有输入用户名或密码')
 
