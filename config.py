f20e7a5c6ac6ce4c506c9e805f0dd4ca1af7d493
yanghuzhi <huzhi.yang@foxmail.com>
2012-04-28 10:50:28 +0800

    fix:database

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DEBUG = True
 SECRET_KEY = '232323sljf209302nadnoasfo2031012'
 
 DATABASE_URI = 'mysql+mysqldb://root:data@localhost:3306/yamler_development?charset=utf8'
-DATABASE_CONNECT_OPTIONS = {}
+DATABASE_CONNECT_OPTIONS = {'pool_recycle':7200}
 
 #ADMINS = frozenset(['http://admin.yamler.com/']
 
--- yamler/database.py
+++ yamler/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from yamler import app
 
-#engine = create_engine(app.config['DATABASE_URI'], convert_unicode=True,**app.config['DATABASE_CONNECT_OPTIONS'])
-engine = create_engine(app.config['DATABASE_URI'], convert_unicode=True, pool_recycle=3600)
+engine = create_engine(app.config['DATABASE_URI'], convert_unicode=True,**app.config['DATABASE_CONNECT_OPTIONS'])
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine)) 
 
 def init_db():
