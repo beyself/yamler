e3e0e4a95126997f83abe9e50bc504bc1c1d80e1
yanghuzhi <huzhi.yang@foxmail.com>
2012-04-28 11:27:58 +0800

    fix:poolclass=NullPool

--- yamler/database.py
+++ yamler/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.pool import NullPool
 from yamler import app
 
-engine = create_engine(app.config['DATABASE_URI'], convert_unicode=True,**app.config['DATABASE_CONNECT_OPTIONS'])
+engine = create_engine(app.config['DATABASE_URI'], poolclass=NullPool, convert_unicode=True,**app.config['DATABASE_CONNECT_OPTIONS'])
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine)) 
 
 def init_db():
