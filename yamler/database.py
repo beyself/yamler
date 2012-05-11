17a6fd7bc4b906cea0d485614e7a9cded9e60ad5
yanghuzhi <huzhi.yang@foxmail.com>
2012-05-11 14:22:34 +0800

    fix

--- yamler/database.py
+++ yamler/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.pool import NullPool
@@ -6,6 +6,7 @@ from yamler import app
 
 engine = create_engine(app.config['DATABASE_URI'], poolclass=NullPool, convert_unicode=True,**app.config['DATABASE_CONNECT_OPTIONS'])
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine)) 
+conn = engine.connect()
 
 def init_db():
     # import all modules here that might define models so that
@@ -16,3 +17,4 @@ def init_db():
 #Model = declarative_base()
 Model = declarative_base(name='Model')
 Model.query = db_session.query_property()
+metadata = MetaData(bind=engine)
